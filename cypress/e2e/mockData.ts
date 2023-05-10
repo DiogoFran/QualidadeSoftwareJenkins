9d5d20b45f2c4afa961a5ff845b0021919b7df4b
DiogoFran <51442304+DiogoFran@users.noreply.github.com>
2023-05-10 16:24:57 +0100

    intial commit

---
+++ cypress/e2e/mockData.ts
@@ -0,0 +1,23 @@
+import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
+
+
+    Given('the user opens the application', () => {
+        cy.visit('https://example.cypress.io/todo')
+    });
+  
+    Given('the todo list is empty', () => {
+  
+    });
+  
+    When("the user clicks on the (.*) button", () => {
+  
+    });
+  
+    Then("the todo list gets (.*) new items", () => {
+  
+    });
+  
+    Then("the (.*) button disappears", () => {
+  
+    });
+
